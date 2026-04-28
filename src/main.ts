2ca71dbefac47aa7538eace0f75b08bea41a7049
Joshua Bascos <joshuabascos@gmail.com>
2026-04-29 06:51:23 +0800

    feat(routes): lazy-load all features with per-route titles

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import { appConfig } from './app/app.config';
 
-
-bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
